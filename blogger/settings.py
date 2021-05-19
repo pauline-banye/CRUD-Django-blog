480cd15e56ff18604646aab3e86c34dcc6961912
pauline-banye <paulinebanye@gmail.com>
2021-05-19 12:33:25 +0100

    updatetests2

--- blogger/settings.py
+++ blogger/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY', 'Optional default value')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 #DEBUG = os.getenv('DEBUG')
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
--- paulinsblog/tests.py
+++ paulinsblog/tests.py
@@ -1,4 +1,4 @@
-#from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
 
 #from django.core.urlresolvers import resolve
 from django.http import HttpRequest
@@ -8,13 +8,53 @@ from django.urls import reverse, reverse_lazy
 from . import views
 
 #from django.utils import timezone
-#from .models import Post, Comment, UserProfile
+from .models import Post, UserProfile  #, Comment,
 #from .forms import BlogForms, CommentForm
 #from user.forms import UserRegForm, UpdateUserForm, ChangepassForm
 
 
 # Create your tests here.
 
+class Test_HomePage(TestCase):
+
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse ('home'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_homepage_uses_correct_template(self):
+        response = self.client.get(reverse ('home'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_homepage_contains_correct_html(self):
+        response = self.client.get('/')
+        self.assertContains(response, '<h2 padding: 70px;><b>Articles</b></h2>')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(response, 'Nothing here')
+
+class Test_New_Post(TestCase):
+
+    def test_new_post_status_code(self):
+        response = self.client.get('/new_post/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse ('new_post'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_new_post_uses_correct_template(self):
+        response = self.client.get(reverse ('new_post'))
+        self.assertTemplateUsed(response, 'new_post.html')
+
+    def test_new_post_does_not_contain_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(response, 'Nothing here')
 
 class Test_Postdetails(TestCase):
 
@@ -22,13 +62,9 @@ class Test_Postdetails(TestCase):
         response = self.client.get('/postdetails/<int:pk>/')
         self.assertEquals(response.status_code, 404)
 
-    def test_view_url_by_name(self):
-        response = self.client.get(reverse_lazy('postdetails/<int:pk>/'))
-        self.assertEqual(response.status_code, 200)
-
-    #def test_postdetails_uses_correct_template(self):
-    #    response = self.client.get(reverse_lazy('/postdetails/<int:pk>/'))
-    #    self.assertTemplateUsed(response, 'postdetails.html')
+    def test_postdetails_uses_correct_template(self):
+        response = self.client.get('/postdetails/<int:pk>/')
+        self.assertTemplateUsed(response, 'postdetails.html')
 
     def test_postdetails_does_not_contain_incorrect_html(self):
         response = self.client.get('/')
